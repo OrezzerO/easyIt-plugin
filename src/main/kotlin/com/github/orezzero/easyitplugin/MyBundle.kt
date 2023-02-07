5543d531ce24c519aa2b76552f63ded04844d8fe
GitHub Action <action@github.com>
2023-02-07 04:31:01 +0000

    Template cleanup

--- src/main/kotlin/com/github/orezzero/easyitplugin/MyBundle.kt
+++ src/main/kotlin/com/github/orezzero/easyitplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.orezzero.easyitplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/orezzero/easyitplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/orezzero/easyitplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.orezzero.easyitplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.orezzero.easyitplugin.MyBundle
 
 class MyApplicationService {
 
