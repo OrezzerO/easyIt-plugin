858e8a466c275e448ccd42c0a6406897ff49b850
orez <orezsilence@163.com>
2023-02-20 09:50:17 +0800

    add test Action to test replace text using  psi element

---
+++ src/main/kotlin/com/github/orezzero/easyitplugin/action/test/TestAction.kt
@@ -0,0 +1,39 @@
+package com.github.orezzero.easyitplugin.action.test
+
+import com.github.orezzero.easyitplugin.runUndoTransparentWriteAction
+import com.github.orezzero.easyitplugin.util.FileUtils.findFileByRelativePath
+import com.github.orezzero.easyitplugin.util.MarkdownElementUtils
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiManager
+import com.intellij.psi.PsiRecursiveElementWalkingVisitor
+
+class TestAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project
+        val virtualFile = findFileByRelativePath(project!!, "README.md")
+        val file = PsiManager.getInstance(project).findFile(virtualFile!!)
+        val elementMap: MutableMap<String, MutableList<PsiElement>> = HashMap()
+        file!!.accept(object : PsiRecursiveElementWalkingVisitor() {
+            override fun visitElement(element: PsiElement) {
+                val name = element.javaClass.name
+                println(name)
+                val psiElements = elementMap.computeIfAbsent(name) { ArrayList() }
+                psiElements.add(element)
+                super.visitElement(element)
+            }
+        }
+        )
+        println(elementMap.size)
+        var list = elementMap.get("org.intellij.plugins.markdown.lang.psi.impl.MarkdownLinkDestination")!!
+        runUndoTransparentWriteAction {
+            for (l in list) {
+                var inlineLink = MarkdownElementUtils.createLineDestination(project, " src/a/b/c/A.kt#L13")
+                l.replace(inlineLink)
+            }
+        }
+
+
+    }
+}
--- src/main/kotlin/com/github/orezzero/easyitplugin/util/MarkdownElementUtils.kt
+++ src/main/kotlin/com/github/orezzero/easyitplugin/util/MarkdownElementUtils.kt
@@ -1,7 +1,12 @@
 package com.github.orezzero.easyitplugin.util
 
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFileFactory
 import com.intellij.psi.tree.TokenSet
+import org.intellij.plugins.markdown.lang.MarkdownFileType
 import org.intellij.plugins.markdown.lang.MarkdownTokenTypes
+import org.intellij.plugins.markdown.lang.psi.impl.MarkdownFile
+import org.intellij.plugins.markdown.lang.psi.impl.MarkdownInlineLink
 import org.intellij.plugins.markdown.lang.psi.impl.MarkdownLinkDestination
 import org.intellij.plugins.markdown.lang.psi.impl.MarkdownLinkText
 
@@ -22,4 +27,25 @@ object MarkdownElementUtils {
         }
         return linkName ?: "EMPTY"
     }
+
+
+    fun createInlineLink(project: Project, name: String): MarkdownInlineLink {
+        val file = createFile(project, name)
+        return file.firstChild.firstChild.firstChild as MarkdownInlineLink
+    }
+
+    fun createFile(project: Project, text: String): MarkdownFile {
+        val name = "dummy.md"
+        return PsiFileFactory.getInstance(project)
+            .createFileFromText(name, MarkdownFileType.INSTANCE, text) as MarkdownFile
+    }
+
+    fun createInlineLink(project: Project, name: String, link: String): MarkdownInlineLink {
+        return createInlineLink(project, "[$name]($link)")
+    }
+
+    fun createLineDestination(project: Project, dest: String): MarkdownLinkDestination {
+        return createInlineLink(project, "[TEST]($dest)").linkDestination!!
+    }
+
 }
